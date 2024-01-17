7be48b4b8c26d1a58d50d3e52da801312311f668
Mael Primet <mael.primet@gmail.com>
2024-01-17 17:51:51 +0100

    feat: update code

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,10 @@
         <ion-content>
           <ion-list id="labels-list">
             <ion-list-header>Labels</ion-list-header>
-
             <ion-item
               v-for="id in ['1', '2', '3', '4']"
               lines="none"
+              style="cursor: pointer"
               :router-link="`/view/${id}`"
               :key="id"
             >
@@ -34,68 +34,6 @@ import {
   IonRouterOutlet,
   IonSplitPane,
 } from "@ionic/vue";
-import { ref } from "vue";
-import {
-  archiveOutline,
-  archiveSharp,
-  heartOutline,
-  heartSharp,
-  mailOutline,
-  mailSharp,
-  paperPlaneOutline,
-  paperPlaneSharp,
-  trashOutline,
-  trashSharp,
-  warningOutline,
-  warningSharp,
-} from "ionicons/icons";
-
-const selectedIndex = ref(0);
-const appPages = [
-  {
-    title: "Inbox",
-    url: "/folder/Inbox",
-    iosIcon: mailOutline,
-    mdIcon: mailSharp,
-  },
-  {
-    title: "Outbox",
-    url: "/folder/Outbox",
-    iosIcon: paperPlaneOutline,
-    mdIcon: paperPlaneSharp,
-  },
-  {
-    title: "Favorites",
-    url: "/folder/Favorites",
-    iosIcon: heartOutline,
-    mdIcon: heartSharp,
-  },
-  {
-    title: "Archived",
-    url: "/folder/Archived",
-    iosIcon: archiveOutline,
-    mdIcon: archiveSharp,
-  },
-  {
-    title: "Trash",
-    url: "/folder/Trash",
-    iosIcon: trashOutline,
-    mdIcon: trashSharp,
-  },
-  {
-    title: "Spam",
-    url: "/folder/Spam",
-    iosIcon: warningOutline,
-    mdIcon: warningSharp,
-  },
-];
-
-const path = window.location.pathname.split("folder/")[1];
-if (path !== undefined) {
-  selectedIndex.value = appPages.findIndex(
-    (page) => page.title.toLowerCase() === path.toLowerCase()
-  );
-}
 </script>
 
 <style scoped>
